a9e0545b1dbdf8b050af8d3f3c1612de48953eda
oasumi <tkkn0701tkkn0701@gmail.com>
2024-09-16 22:00:07 +0900

    indexのビューファイルを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'furimas/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "furimas#index"
 end
