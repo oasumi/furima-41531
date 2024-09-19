445b45a54f5c451612dcae6fe1cd283795e7789b
oasumi <tkkn0701tkkn0701@gmail.com>
2024-09-19 20:21:59 +0900

    Rubocop②

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname { 'asumi' }
-    email    {Faker::Internet.email}
+    email    { Faker::Internet.email }
     password { 'test1234' }
     password_confirmation { password }
     last_name { '小野' }
