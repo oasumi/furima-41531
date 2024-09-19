a6b6a34dd65385eb7bcd6550365fc26d01b0ad6f
oasumi <tkkn0701tkkn0701@gmail.com>
2024-09-19 13:34:25 +0900

    正規表現の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
+with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  validates :first_name
+  validates :last_name
+end
+
+with_options presence: true, format: { with: /\A[\p{katakana}ー－]+\z/, message: '全角カタカナを使用してください' } do
+  validates :first_name_kana
+  validates :last_name_kana
+end
 
   validates :nickname, presence: true
   validates :last_name, presence: true
@@ -11,6 +23,7 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birth_date, presence: true
+
 
 
 end
