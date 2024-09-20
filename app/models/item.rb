9fbd7a5aca1049f9b8cafd294aa658573b029910
oasumi <tkkn0701tkkn0701@gmail.com>
2024-09-20 10:55:34 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,8 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   
 
+  validates :image,:item_name, :item_description, :item_price: true
+
+  validates :category_id, :condition_id, :shipping_fee_payer_id, :shipping_origin_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" } 
+
 end
