bc0e7793f9569b920f5a0bd1343818160c258433
oasumi <tkkn0701tkkn0701@gmail.com>
2024-09-20 10:36:09 +0900

    ActiveHashの導入～アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_payer
+  belongs_to :shipping_origin
+  belongs_to :shipping_day
+  
+
 end
