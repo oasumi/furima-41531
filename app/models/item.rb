f102202a07d870b010cbc88e74ccadabe3578023
oasumi <tkkn0701tkkn0701@gmail.com>
2024-09-22 05:14:33 +0900

    テストコードの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,9 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   
 
-  validates :image, :item_name, :item_description, :item_price, presence: true
+  validates :image, :item_description, :item_price, presence: true
+  validates :item_name, presence: true, length: { maximum: 40 }
+  validates :item_description, length: { maximum: 1000 }
 
   validates :category_id, :condition_id, :shipping_fee_payer_id, :shipping_origin_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" } 
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+
+    context '内容に問題ない場合' do
+      it 'すべての情報があれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'item_nameが空だと保存できないこと' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+
+      it 'item_nameが41文字以上では登録できない' do
+        @item.item_name =  Faker::Lorem.characters(number: 41)
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name is too long (maximum is 40 characters)")
+      end
+
+      it 'item_descriptionが空だと保存できないこと' do
+        @item.item_description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item description can't be blank")
+      end
+
+      it 'item_descriptionが1001文字以上では登録できない' do
+        @item.item_description =  Faker::Lorem.characters(number: 1001)
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item description is too long (maximum is 1000 characters)")
+      end
+
+      it 'category_idが空だと保存できないこと' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it 'condition_idが空だと保存できないこと' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+
+      it 'shipping_fee_payer_idが空だと保存できないこと' do
+        @item.shipping_fee_payer_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee payer can't be blank")
+      end
+
+      it 'shipping_origin_idが空だと保存できないこと' do
+        @item.shipping_origin_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping origin can't be blank")
+      end
+
+      it 'shipping_day_idが空だと保存できないこと' do
+        @item.shipping_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+
+      it 'item_priceが空だと保存できないこと' do
+        @item.item_price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price can't be blank")
+      end
+
+      it 'imageが空だと保存できないこと' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+
+    end
+  end
 end
